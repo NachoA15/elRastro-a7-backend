07b7f2b27bf87cb8a1e1e85d302ea377e82cb531
Nacho <100839254+NachoA15@users.noreply.github.com>
2024-01-02 16:14:23 +0100

    Primera versión

---
+++ productos-module/db/model/producto.js
@@ -0,0 +1,17 @@
+const {Schema, model} = require('mongoose')
+const mongoose = require('mongoose')
+const productoSchema = new Schema({
+    nombre: String,
+    direccion: Number,
+    usuario: String,
+    precioInicial: Number,
+    fechaInicio: Date,
+    fechaCierre: Date,
+    descripcion: String,
+    imagen: String,
+    pagado: Boolean,
+    puja: JSON
+})
+
+
+module.exports = mongoose.models.Producto || mongoose.model('Producto', productoSchema)
